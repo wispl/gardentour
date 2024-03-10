d4be035a1d9a238c507292992ca70e2b77cee3ff
wispl <wispl.8qbkk@slmail.me>
2024-03-10 00:58:10 -0500

    feat: replace soundex search with substring search
    Soundex search did not work too well as a good portion
    of the names were generic towns so a regular substring search is better, even if probably less performant.

--- app/src/main/java/com/example/app/data/SearchRepository.kt
+++ app/src/main/java/com/example/app/data/SearchRepository.kt
@@ -1,12 +1,10 @@
 package com.example.app.data
 
 object SearchRepository {
-    // Soundex key mapped to names of places it matched
-    // Each word of the place's name is encoded and merged together
-    private val placesSoundexMap = PlaceRepository.allPlaces()
-        .map { place -> place.name.split(" ")
-            .map { soundexEncodeWord(it) to place.name }
-        }
+    // Splits place names into words and group them, forming buckets to reduce
+    // searching the full repository
+    private val placesBucketMap = PlaceRepository.allPlaces()
+        .map { place -> place.name.split(" ").map { it.lowercase() to place } }
         .flatten()
         .groupBy({ it.first }, {it.second})
 
@@ -14,52 +12,10 @@ object SearchRepository {
         if (str.isEmpty()) {
             return emptyList()
         }
-        return soundexEncode(str)
-            .map { placesSoundexMap.getOrDefault(it, emptyList()) }
+        return placesBucketMap
+            .filter { str.lowercase() in it.key }
+            .values
             .flatten()
-            .map { PlaceRepository.get(it) }
-    }
-
-    private fun soundexEncode(str: String): List<String> {
-        assert(str.isNotEmpty())
-        return str.split(" ").map { soundexEncodeWord(it) }
-    }
-
-    private fun soundexEncodeWord(word: String): String {
-        assert(word.isNotEmpty())
-
-        val str = word.lowercase()
-        var result = ""
-        var prev: Char? = null
-
-        for (i in str) {
-            var mapping = soundexMap(i)
-            if (result.isEmpty()) {
-                result += str.first().uppercase()
-            } else if (mapping == '0') {
-                mapping = null
-            } else if (mapping != null && mapping != prev) {
-                result += mapping
-            } else {
-                mapping = prev
-            }
-            prev = mapping
-        }
-
-        return result.padEnd(4, '0').substring(0, 4)
-    }
-
-    private fun soundexMap(c: Char): Char? {
-        return when (c) {
-            'a', 'e', 'i', 'o', 'u' -> '0'
-            'b', 'f', 'p', 'v' -> '1'
-            'c', 'g', 'j', 'k', 'q', 's', 'x', 'z' -> '2'
-            'd', 't' -> '3'
-            'l' -> '4'
-            'm', 'n' -> '5'
-            'r' -> '6'
-            // w, h, y are handled here as well
-            else -> null
-        }
+            .distinct()
     }
 }
